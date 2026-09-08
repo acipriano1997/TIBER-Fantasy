d5fa47ca8488ffadae1254e6ebed48fa202c2954
acipriano1997 <anthonyscipriano@gmail.com>
2026-09-07 20:17:53 -0400

    Gate 4: load shared mobile PWA hardening

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./styles/mobilePwa.css";
 
 // Set dark mode globally for all shadcn components
 document.documentElement.classList.add("dark");
