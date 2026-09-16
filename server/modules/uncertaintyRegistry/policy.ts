4dd553a4e8521d62bab567eb342198ef83957b1f
acipriano1997 <anthonyscipriano@gmail.com>
2026-09-16 11:40:20 -0400

    refactor(ffcc): route USR attention through corrected policy

--- server/modules/uncertaintyRegistry/policy.ts
+++ server/modules/uncertaintyRegistry/policy.ts
@@ -1,1 +1,15 @@
-export * from './policyCore';
+export {
+  classifySituationResolutionV0,
+  evaluateWitnessV0,
+  isSituationEvidenceEligibleAtV0,
+  shouldReevaluateSituationV0,
+  validateScenarioBranchBindingV0,
+} from './policyCore';
+export type {
+  ReevaluationDecisionV0,
+  ResolutionAssessmentV0,
+  ScenarioBranchValidationV0,
+  SharedAttentionAssessmentV0,
+  StateSupportAssessmentV0,
+} from './policyCore';
+export { classifySharedAttentionV0 } from './attentionPolicy';
